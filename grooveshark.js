4d79fbe02f4c210ca65a6edfb06a9d8daabced2e
Julien Delnatte <julien.delnatte@gmail.com>
2014-08-18 19:08:00 +0200

    - add shuffle mode - new Air version - add like and unlike button - add auto fade volume when Sarah speak (with all plugin)

--- grooveshark.js
+++ grooveshark.js
@@ -6,16 +6,16 @@ var grooveSharkProcess;
 exports.speak = function(tts, async, SARAH)
 { 
 	if(grooveSharkProcess)
-		grooveSharkProcess = exec(API_URL+"sarahSay "+tts, function (error, stdout, stderr){});
-	//console.log("say somethings : "+tts);
+		grooveSharkProcess = exec(API_URL+" speak "+tts, function (error, stdout, stderr){});
 	return tts;
 }
 */
 exports.action = function(data, callback, config, SARAH)
-{
+{	
 	var path = require("path").resolve(".");
 	config = config.modules.grooveshark;
-	
+		
+	var needProcess = false;
 	var params = "";
 	var tts = "Je m'en occupe";
 	switch(data.command)
@@ -32,25 +32,31 @@ exports.action = function(data, callback, config, SARAH)
 			tts="";
 			console.log("coucou");
 			params = "setVolume "+data.value;
+			needProcess = true;
 			break;
 		case "pause":
 			tts="";
 			params = "pause";
+			needProcess = true;
 			break;
 		case "resume":
 			tts="";
 			params = "resume";
+			needProcess = true;
 			break;
 		case "next":
 			tts="";
 			params = "next";
+			needProcess = true;
 			break;
 		case "close":
 			params = "close";
+			needProcess = true;
 			break;
 		case "prev":
 			tts="";
 			params = "prev";
+			needProcess = true;
 			break
 		case "updatePlaylist":
 			params = "updatePlaylist";
@@ -99,6 +105,12 @@ exports.action = function(data, callback, config, SARAH)
 		case "like":
 			tts="";
 			params = "like";
+			needProcess = true;
+			break;
+		case "unlike":
+			tts="";
+			params = "unlike";
+			needProcess = true;
 			break;
 		case "favorites":
 			params = "favorites";
@@ -112,6 +124,7 @@ exports.action = function(data, callback, config, SARAH)
 				params = "currentSongAlbum";
 			else
 				params = "currentSong";
+			needProcess = true;
 			break;
 		case "popular":
 			params = "popular";
@@ -119,6 +132,17 @@ exports.action = function(data, callback, config, SARAH)
 		case "currently":
 			tts="";
 			params = "currently";
+			needProcess = true;
+			break;
+		case "shuffleTrue":
+			tts="";
+			params = "shuffle true";
+			needProcess = true;
+			break;
+		case "shuffleFalse":
+			tts="";
+			params = "shuffle false";
+			needProcess = true;
 			break;
 		default : 
 			callback();
@@ -126,10 +150,15 @@ exports.action = function(data, callback, config, SARAH)
 			break;
 	}
 
-	callback({"tts":tts});
 	
 	if(grooveSharkProcess == null)
+	{
+		if(needProcess == true)
+			return 	callback({"tts":"Grouvshark n'est pas lancé"});
 		exec(API_URL+" config "+config.login+" "+config.password+" "+config.bigPicture);
+	}
+	
+	callback({"tts":tts});
 
 	grooveSharkProcess = exec(API_URL+params,
 		function (error, stdout, stderr) {
